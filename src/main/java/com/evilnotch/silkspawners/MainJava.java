7fe91f1153fff46bd2fc78f82c8939fc7a3d074f
jredfox <jjredfox@hotmail.com>
2019-03-15 06:27:41 -0500

    fix @mod

--- src/main/java/com/evilnotch/silkspawners/MainJava.java
+++ src/main/java/com/evilnotch/silkspawners/MainJava.java
@@ -46,11 +46,11 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
-@Mod(modid = MainJava.MODID,name = "silkspawners", version = MainJava.VERSION,acceptableRemoteVersions = "*", dependencies = "required-after:evilnotchlib@[1.2.3]")
+@Mod(modid = MainJava.MODID,name = "silkspawners", version = MainJava.VERSION,acceptableRemoteVersions = "*", dependencies = "required-after:evilnotchlib@[1.2.3,]")
 public class MainJava
 {
     public static final String MODID = "silkspawners";
-    public static final String VERSION = "1.8.1.05";
+    public static final String VERSION = "1.8.1.06";
 	@SidedProxy(clientSide = "com.evilnotch.silkspawners.client.proxy.ClientProxy", serverSide = "com.evilnotch.silkspawners.client.proxy.ServerProxy")
 	public static ServerProxy proxy;
 	public static String[] versionType = {"Beta","Alpha","Release","Indev","WIPING"};
